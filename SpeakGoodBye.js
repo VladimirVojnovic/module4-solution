d63267d0de974d679a467f3f4e05346dc72d036a
Vladimir Vojnović <vladavoj@gmail.com>
2016-08-23 12:11:52 +0200

    Add files via upload

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -15,17 +15,16 @@
 
 var speakWord = "Good Bye";
 var byeSpeaker = {};
-// byeSpeaker.name = string;
 
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
 byeSpeaker.speak = function (name) {
   console.log(speakWord + " " + name);
-}
+};
 
-window.byeSpeaker = byeSpeaker;	
-})(window);
+window.byeSpeaker = byeSpeaker;
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
 // xxxx.xxxx = byeSpeaker;
+})(window);
--- SpeakHello.js
+++ SpeakHello.js
@@ -8,8 +8,8 @@
 // var helloSpeaker =
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
-(function (window) {
-	
+(function (window){
+
 var speakWord = "Hello";
 var helloSpeaker = {};
 
@@ -18,13 +18,13 @@ var helloSpeaker = {};
 // See Lecture 52, part 2
 helloSpeaker.speak = function (name) {
   console.log(speakWord + " " + name);
-}
+};
 
 window.helloSpeaker = helloSpeaker;
-})(window);
-
 // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
 // 'helloSpeaker' on the global scope as well.
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
 // xxxx.xxxx = helloSpeaker;
+
+})(window);
